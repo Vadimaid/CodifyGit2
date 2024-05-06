4bff254f88103f8203bb7006b957c709b703efaa
vadim <vadimaid@gmail.com>
2024-05-06 20:49:39 +0600

    My first commit

---
+++ src/main/java/com/example/codifygit2/CodifyGit2Application.java
@@ -0,0 +1,13 @@
+package com.example.codifygit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodifyGit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodifyGit2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codifygit2/CodifyGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codifygit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodifyGit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
