9ee92efc80e077792181c00a7a6042a33f40d8f2
vadim <vadimaid@gmail.com>
2024-05-08 20:16:42 +0600

    Added new controller

--- src/main/java/com/example/codifygit2/controller/TestController.java
+++ src/main/java/com/example/codifygit2/controller/TestController.java
@@ -1,2 +1,16 @@
-package com.example.codifygit2.controller;public class TestController {
+package com.example.codifygit2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class TestController {
+
+    @GetMapping(value = "/test-1")
+    public String sayHello() {
+        return "Hello world";
+    }
+
 }
