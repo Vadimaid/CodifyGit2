79b91805d4cf89e3f6ac5518e1bd13a023a519c0
vadim <vadimaid@gmail.com>
2024-05-10 20:07:40 +0600

    CODIFY-TEST: Added new endpoint

--- src/main/java/com/example/codifygit2/controller/TestController.java
+++ src/main/java/com/example/codifygit2/controller/TestController.java
@@ -2,7 +2,10 @@ package com.example.codifygit2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -11,6 +14,16 @@ public class TestController {
     @GetMapping(value = "/test-1")
     public String sayHello() {
         return "Hello world";
+    }
+
+    @GetMapping(value = "/test-10")
+    public String sayHello10(
+            @RequestParam(value = "name", required = false) String name
+    ) {
+        if (Objects.isNull(name)) {
+            return "Hello world";
+        }
+       return "Hello " + name;
     }
 
 }
